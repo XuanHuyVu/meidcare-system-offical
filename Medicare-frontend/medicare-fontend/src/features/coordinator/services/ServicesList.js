a4d752ce7ed285d7c06c70cfea6b5a7956633e06
QuangHieu0711 <nguyenquanghieugl2019@gmail.com>
2025-06-16 10:00:18 +0700

    Update Services Delete

--- Medicare-frontend/medicare-fontend/src/features/coordinator/services/ServicesList.js
+++ Medicare-frontend/medicare-fontend/src/features/coordinator/services/ServicesList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { faInfoCircle, faEdit, faTrash, faSearch } from '@fortawesome/free-solid-svg-icons';
 import '../../../style/Services.css';
 import { GetAllAsync, CreateAsync, UpdateAsync, DeleteAsync } from '../../../api/ServicesApi';
