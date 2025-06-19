ae0f6180f204d5713b67b6843f4b392a95a6f0fe
ThuyLienLuong <luonglien332@gmail.com>
2025-06-19 11:10:56 +0700

    update Facade_Observer

---
+++ Medicare-backend/Medicare-backend/Medicare-backend/Services/Implementations/MedicalRecordService.cs
@@ -0,0 +1,12 @@
+using Medicare_backend.Services.Interfaces;
+
+namespace Medicare_backend.Services.Implementations
+{
+    public class MedicalRecordService : IMedicalRecordService
+    {
+        public void AddMedicalRecord(int patientId, string diagnosis)
+        {
+            // TODO: Implement logic here
+        }
+    }
+} 
