ec52a6c79c175a989fb0385c7ce9b8df650e53a0
VictorFlorence <vuhuy25092004@gmail.com>
2025-06-20 09:07:15 +0700

    update README.md

--- Medicare-backend/Medicare-backend/Medicare-backend/obj/Debug/net8.0/Medicare_backend.AssemblyInfo.cs
+++ Medicare-backend/Medicare-backend/Medicare-backend/obj/Debug/net8.0/Medicare_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medicare_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea375ecb4f4e6d6509e2072f5b4991484cd0e03a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0462ae4e33f8dae638f18c17728a206ffb051a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medicare_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medicare_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
